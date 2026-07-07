13554f1054c0d92dcc4fe0362a2c776d87b3e2e1
Sythe120 <nontiveros15@icloud.com>
2026-07-07 00:28:30 -0500

    Update alert message with iCloud Shortcut link

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-document.querySelector('.btn').onclick=e=>{e.preventDefault();alert('Replace this with your iCloud Shortcut link!')}
+document.querySelector('.btn').onclick=e=>{e.preventDefault();alert('https://www.icloud.com/shortcuts/0fdbc84599d6480bb724712a65ce281e')}
